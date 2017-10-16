2becd3e7ad2ad1f2f98c360f64cbc8e92b390a73
Erin Leck <erinleck@gmail.com>
2017-10-16 16:48:04 +0000

    checkpoint 21

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :topics
-  resources :posts
-
-  get 'about' => 'welcome#about'
+  resources :topics do
+    resources :posts, except: [:index]
+  end
   
-  get 'welcome/contact'
+  get 'about' => 'welcome#about'
   
   root 'welcome#index'
 end
