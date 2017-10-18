918ee89db719821ef9ab4b7b16a3812ba478f2c0
Erin Leck <erinleck@gmail.com>
2017-10-18 22:43:00 +0000

    checkpoint 23 authentication assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user_lower_case) { User.create!(name: "erin leck", email: "erin@bloccit.com", password: "password") }
   
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -18,6 +19,12 @@ RSpec.describe User, type: :model do
   describe "attributes" do
     it "should have name and email attributes" do
         expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    end
+  end
+  
+  describe "user name requirements" do
+    it "should capitalize first and last names" do
+        expect(user_lower_case.name).to eq("Erin Leck")
     end
   end
   
