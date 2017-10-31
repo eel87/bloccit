0fa8dd244e3045bf45380e1d10aa1fd611df3e47
Erin Leck <erinleck@gmail.com>
2017-10-31 21:18:55 +0000

    posts and users refactor failures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,47 @@
 require 'random_data'
 
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
+
 15.times do
-    Topic.create!(
-        name: RandomData.random_sentence,
-        description: RandomData.random_paragraph
-        )
-    end
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+    )
+  end
 topics = Topic.all
 
 50.times do
-    Post.create!(
-        topic: topics.sample,
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph
-        )
+  Post.create!(
+    user: users.sample,
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+    )
 end
 posts = Post.all
 
 100.times do
-    Comment.create!(
-        post: posts.sample,
-        body: RandomData.random_paragraph
-        )
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+    )
 end
+
+user = User.first
+user.update_attributes!(
+  email: 'erinleck@gmail.com',
+  password: 'helloworld'
+  )
     
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topic created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
