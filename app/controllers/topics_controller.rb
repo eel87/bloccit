a948f9e698d05a4df300fdfedc761f9371a9d62b
Erin Leck <erinleck@gmail.com>
2017-11-01 20:14:33 +0000

    checkpoint 27 assignment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
   
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, excpet: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_moderator, except: [:show, :update]
   
   def index
     @topics = Topic.all
@@ -62,6 +63,13 @@ class TopicsController < ApplicationController
   
   def authorize_user
     unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
+  
+  def authorize_moderator
+    unless current_user.moderator?
       flash[:alert] = "You must be an admin to do that."
       redirect_to topics_path
     end
