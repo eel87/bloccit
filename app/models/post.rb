bd08afafd7bc869becf9cc033167f8af7f32c7c9
Erin Leck <erinleck@gmail.com>
2017-11-01 17:15:32 +0000

    checkpoint 26 corrected assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,12 @@
 class Post < ApplicationRecord
   
-  #ordered_by_title scope
-  scope :ordered_by_title, -> { where(ordered_by_title: true) }
-  
-  #ordered_by_reverse_created_at scope
-  scope :ordered_by_reverse_created_at, -> { where(ordered_by_reverse_created_at: true) }
-  
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
   
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('title DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
