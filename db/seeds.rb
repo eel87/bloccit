1dcdc339a5097f85e1f0d341bbac10f55b385dd3
Erin Leck <erinleck@gmail.com>
2017-11-01 18:55:52 +0000

    checkpoint 27

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,12 +34,19 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'erinleck@gmail.com',
-  password: 'helloworld'
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
   )
-    
+
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld'
+  )  
+  
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topic created"
